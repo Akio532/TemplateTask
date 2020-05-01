c5a73216f1949068fed377b8997773aec5b1ea1a
Akio532 <60925360+Akio532@users.noreply.github.com>
2020-05-01 22:06:21 +0300

    Add files via upload

---
+++ Practice3/Task1/Task1.cpp
@@ -0,0 +1,24 @@
+﻿// Task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "queuequeue.h"
+//include <queuequeue>
+using namespace std;
+
+int main()
+{
+	Queue<int> queue1 = Queue <int>(1);
+	queue1.push(1);
+	Queue<int> queue2 = Queue<int>(5);
+	queue2.push(2);
+	queue2.push(3);
+	queue2.push(4);
+	queue2.push(5);
+	queue2.push(6);
+	Queue<int> queue3 = queuing(queue1, queue2);
+	for (int i = 0; i < 5; i++)
+		cout << queue3.pop() << endl;
+	return 0;
+    //std::cout << "Hello World!\n";
+}
